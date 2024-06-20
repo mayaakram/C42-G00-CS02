39c69ec092205a4a9323b86059e9c1e698cdc3a1
MAYA <MAYA@example.com>
2024-06-20 23:50:11 +0200

    ques1

---
+++ assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region ques1
+            //1 - Write a program that allows the user to enter a number then print it.
+            //int Number;     //Number = int.Parse(Console.ReadLine() ?? "0");
+            //Console.WriteLine(Number);
+            #endregion
+
+        }
+    }
+}
