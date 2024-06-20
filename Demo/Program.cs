8cac577d742b35dab14b85fd8c37e5293d92cc59
MAYA <MAYA@example.com>
2024-06-20 23:31:16 +0200

    operators

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -34,7 +34,65 @@ namespace Day_02
             //Console.WriteLine(Number);
             #endregion
 
-            
+
+            #region operators
+            #region unary operator
+            /////unary operator (works on one operand(variable))     //int x = 10;
+            /////1 ++     /////1.1 prefix [increment and then print]
+            //Console.WriteLine(++x);// 11     //                       //1.2 postfix [print and then increment]
+            //Console.WriteLine(x++); //10
+            /////2 --
+            /////2.1 prefix //2.2 postfix    
+            #endregion
+            #region binary operator
+            /////binary operator
+            //int Sum, Num01, Num02;     //Sum = Num01 + Num02;
+            //Sum = Num01 - Num02;     //Sum = Num01 * Num02;
+            //Sum = Num01 / Num02;     //Sum = Num01 % Num02;
+
+            #endregion
+            #region assignment operators
+            /////assignment operator     //int X;
+            //X += 4; //x=x+4     //X -= 4;
+            //X *= 4;     //X /= 4;
+            //X %= 4;
+            #endregion
+            #region relational operators[comparisons]
+            /////relational operators
+            //int X = 6, Y = 5;     //Console.WriteLine(X==Y);
+            //Console.WriteLine(X != Y);     //Console.WriteLine(X > Y);
+            //Console.WriteLine(X < Y);     //Console.WriteLine(X >= Y);
+            //Console.WriteLine(X <= Y);
+            #endregion
+
+            #region logical operator    
+            /////logical operator //short circuit
+            //Console.WriteLine(!false);     //Console.WriteLine(false&&true);//short circuit
+            //Console.WriteLine(false || true);
+            #endregion
+
+            #region  bitwise operators[long circuit]
+            //// ///bitwise operators
+            //// Console.WriteLine(~0);     //// Console.WriteLine(false&true); //long circuit
+            //// //               0000 & 0001  =0000     //// Console.WriteLine(true | false);
+            //// /*
+            ////  Complimo 0 =1     ////  Complimo 1=0 
+            //// ~ de alcomplimo
+            //// Al & alwa7da de lazem t check 3al alatnin 7ata lw mn alawel false fana 3aref ano false 
+            ////34an hya lazem ht7wel ala2awel w check 34an t3rf
+            //// Fa lw 3ndy condition ana m7tago ytnfz regardless hw true wala false yb2a hst3ml de
+            //// W al or nfs alklam
+            //// Lazem tnfz 34an ytl3 result t7wlha l binary      //// Fa kda kda al2 conditions hytnfzo
+            ////  */
+            #endregion
+
+            #region ternary operator
+            /////ternary operator     //int X = 10, Y = 5;
+            //string Message = X > Y ? "X is more than Y" : X < Y ? "X is less than Y" : "x equals Y";
+            //Console.WriteLine(Message);
+            #endregion 
+            #endregion
+
 
         }
 
