8ba4f29b60544b631a8b16122db23e1348a27e94
MAYA <you@example.com>
2024-06-20 23:05:38 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace Day_02
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            #region helloworld
+
+            //Console.Write("hello world");
+            //Console.ReadLine();
+
+
+            #endregion
+
+
+
+
+        
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+
+        }
+
+
+
+
+    } 
+
+}
+
+
+
+
