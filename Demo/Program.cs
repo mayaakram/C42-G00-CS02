7bbbaa9c5a31b587753f3f13226dfdb015207cdb
MAYA <MAYA@example.com>
2024-06-20 23:16:35 +0200

    explicit vs implicit casting

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -9,20 +9,21 @@ namespace Day_02
 
         static void Main(string[] args)
         {
-            #region helloworld
 
-            //Console.Write("hello world");
-            //Console.ReadLine();
+            #region value type vs refrence type video
+            //Point P1;
+            /////declare for refrence of type "point" refering to null            /////this refrence 'p1' can refer to an object from type 'point' or other type inherting from 'point'
+            /////CLR wiil allocate 4 bytes for the refrence at stack            /////CLR will allocate 0 bytes at HEAP
 
+            //P1 = new Point();            /////new
+            /////1.allocate required number of bytes in HEAP (object size +CLR overhead variables )            /////2.initialize (cross out) allocated bytes with default value of its datatype
+            /////3. call user-defined constructor if exists            /////4. assign the refrence to a allocated object
+            //Console.WriteLine(P1.X);//0
+            //Console.WriteLine(P1.Y);//0
+            //Point P2 = new Point(); //unreachable object            //P2 = P1;
+            //P2.X = 10;            //Console.WriteLine(P1.X);
 
             #endregion
-
-
-
-
-        
-
-            /////////////////////////////////////////////////////////////////////////////////
 
 
         }
